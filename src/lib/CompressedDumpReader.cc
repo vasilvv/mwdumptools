479cd9f671a66c86d456db03bf49c8f3e43f332a
Victor Vasiliev <vasilvv@mit.edu>
2014-07-03 16:04:06 -0400

    Support reading dump directly from 7z

--- src/lib/CompressedDumpReader.cc
+++ src/lib/CompressedDumpReader.cc
@@ -1,8 +1,10 @@
 #include "CompressedDumpReader.hh"
 
+#include <cassert>
 #include <cstring>
 #include <fstream>
 
+#include <archive.h>
 #include <zlib.h>
 
 #define BUFFER_SIZE 8192
@@ -62,6 +64,50 @@ class GzipDumpReader : public CompressedDumpReader {
     bool valid() { return file != nullptr; }
 };
 
+class LibarchiveDumpReader : public CompressedDumpReader {
+  private:
+    struct archive *arch;
+    bool err;
+
+  public:
+    LibarchiveDumpReader(const char *path) : CompressedDumpReader() {
+        arch = archive_read_new();
+        archive_read_support_filter_all(arch);
+        archive_read_support_format_all(arch);
+
+        err = archive_read_open_filename(arch, path, 65536) != ARCHIVE_OK;
+        if (err) {
+            return;
+        }
+
+        archive_entry *e;
+        err = archive_read_next_header(arch, &e) != ARCHIVE_OK;
+    }
+
+    virtual ~LibarchiveDumpReader() {
+        if (arch) {
+            archive_read_close(arch);
+            archive_read_free(arch);
+            arch = nullptr;
+        }
+    }
+
+    virtual int get() override {
+        char result;
+        if (read(&result, 1) == 1) {
+            return result;
+        } else {
+            return std::char_traits<char>::eof();
+        }
+    }
+
+    virtual ssize_t read(char *buffer, size_t len) override {
+        return archive_read_data(arch, buffer, len);
+    }
+
+    bool valid() { return !err; }
+};
+
 /**
  * Attempt to determine the type of the file based on the first
  * 10 characters of it.
@@ -88,6 +134,11 @@ CompressedDumpReader_u open_compressed_dump(const char *path) {
     // Gzip
     if (!memcmp("\x1f\x8b\x08", preamble, 3)) {
         return CompressedDumpReader_u(new GzipDumpReader(path));
+    }
+
+    // 7-zip
+    if (!memcmp("7z\xbc\xaf\x27\x1c", preamble, 6)) {
+        return CompressedDumpReader_u(new LibarchiveDumpReader(path));
     }
 
     // Play safe and return error
