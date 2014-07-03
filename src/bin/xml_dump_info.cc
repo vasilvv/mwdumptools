5629fc7490afb116ca75978c19fb3323607c8020
Victor Vasiliev <vasilvv@mit.edu>
2014-07-03 14:38:14 -0400

    Actually exit when called without the argument

--- src/bin/xml_dump_info.cc
+++ src/bin/xml_dump_info.cc
@@ -5,6 +5,7 @@
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         std::cerr << "Usage: mw-xml-dump-info dump.xml" << std::endl;
+        return 1;
     }
 
     XMLDumpParser parser(argv[1], PerPage);
